3b26beb3ad684f2dc5c8570c05a12965bb315f7a
xpippi <pippianders@hotmail.com>
2023-04-10 03:54:14 +0200

    10

---
+++ goto10.go
@@ -0,0 +1,28 @@
+package mainto_test
+
+import "fmt"
+
+func mainto_test() {
+
+	i := 1
+	for i <= 3 {
+		fmt.Println(i)
+		i = i + 1
+	}
+
+	for j := 7; j <= 9; j++ {
+		fmt.Println(j)
+	}
+
+	for {
+		fmt.Println("goto 10")
+		break
+	}
+
+	for n := 0; n <= 5; n++ {
+		if n%2 == 0 {
+			continue
+		}
+		fmt.Println(n)
+	}
+}
